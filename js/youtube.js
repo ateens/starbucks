190852fddfa0a7c62cebac1749c7246b65189666
ateens <atenes8120@gmail.com>
2021-06-19 22:31:43 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,20 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    videoId: "OAyyTrRP_zM",
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: "OAyyTrRP_zM",
+    },
+  });
+}
